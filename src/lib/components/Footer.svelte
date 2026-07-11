093517fb58eb6f7051f69fac43119bced70667ae
Shing Chan <chan.shing@protonmail.com>
2026-07-11 00:35:44 -0700

    refactor(custom-branding): Remove and shorten un-needed footer information in `Footer.svelte`

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -14,14 +14,6 @@
 	<a
 		target="_blank"
 		rel="noopener noreferrer"
-		href="https://www.openstreetmap.org/search?query=San%20Ramon%2C%20CA"
-		class="px-3 text-xs text-subtle-foreground hover:text-muted-foreground transition"
-	>
-		San Ramon, CA
-	</a>
-	<a
-		target="_blank"
-		rel="noopener noreferrer"
 		href="mailto:me@chansh.ing"
 		class="px-3 text-xs text-subtle-foreground hover:text-muted-foreground transition"
 	>
@@ -30,17 +22,9 @@
 	<a
 		target="_blank"
 		rel="noopener noreferrer"
-		href="https://github.com/cnshing"
-		class="px-3 text-xs text-subtle-foreground hover:text-muted-foreground transition"
-	>
-		https://github.com/cnshing
-	</a>
-	<a
-		target="_blank"
-		rel="noopener noreferrer"
 		href="https://linkedin.com/in/shing-chan"
 		class="px-3 text-xs text-subtle-foreground hover:text-muted-foreground transition"
 	>
-		https://linkedin.com/in/shing-chan
+		Linkedin
 	</a>
 </div>
